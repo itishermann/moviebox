0a3c59c94051b233824f72f64cc46481e8aea9ce
Hermann KAO <38349706+HermannKK@users.noreply.github.com>
2019-11-10 21:06:25 +0100

    Lighthouse test update & correction

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="Footer container-fluid py-3">
+    <div className="Footer container py-3">
       <div className="row">
         <div className="copyright col text-center">
-          <div>Made with love with ReactJS, Material UI and TVAPI</div>
+          <div>Made with <span className="heart">&#10084;</span> with <a href="https://reactjs.org/"  target="_blank" rel="noopener noreferrer" aria-label="Get more information">ReactJS</a>, <a href="https://material-ui.com/" target="_blank" rel="noopener noreferrer" aria-label="Get more information">Material UI</a> and <a href="https://www.tvmaze.com/api" target="_blank" rel="noopener noreferrer" aria-label="Get more information">TVAPI</a></div>
           <div>
             Copyright &copy; 2019. All rights
             reserved.
